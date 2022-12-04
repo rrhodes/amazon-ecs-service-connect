637e7ff5f47be76b0758a6b561bb109b3812adfd
Ross Rhodes <trh@trustpilot.com>
2022-12-04 12:04:08 +0000

    Initial commit

---
+++ test/amazon-ecs-service-connect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmazonEcsServiceConnect from '../lib/amazon-ecs-service-connect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amazon-ecs-service-connect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmazonEcsServiceConnect.AmazonEcsServiceConnectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
